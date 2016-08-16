684ad90565e9546381fd31490f63eef8455bcf6f
Joachim Strömbergson <joachim@secworks.se>
2016-08-16 08:50:44 +0200

    Adding logic to perform operations.

--- src/rtl/m6502_alu.v
+++ src/rtl/m6502_alu.v
@@ -42,17 +42,140 @@ module m6502_alu(
                  input wire  [7 : 0] operation,
                  input wire  [7 : 0] op_a,
                  input wire  [7 : 0] op_b,
+                 input wire          carry_in,
                  output wire [7 : 0] result,
-                 output wire         carry,
+                 output wire         carry_out,
                  output wire         zero
                 );
 
   //----------------------------------------------------------------
+  // Defines.
+  //----------------------------------------------------------------
+  localparam OP_AND = 8'h01;
+  localparam OP_OR  = 8'h02;
+  localparam OP_XOR = 8'h03;
+  localparam OP_NOT = 8'h04;
+
+  localparam OP_ASL = 8'h11;
+  localparam OP_ROL = 8'h12;
+  localparam OP_ASR = 8'h13;
+  localparam OP_ROR = 8'h14;
+
+  localparam OP_ADD = 8'h21;
+  localparam OP_INC = 8'h22;
+  localparam OP_SUB = 8'h23;
+  localparam OP_DEC = 8'h24;
+
+  localparam OP_CMP = 8'h31;
+
+
+  //----------------------------------------------------------------
+  // Wires.
+  //----------------------------------------------------------------
+  reg [7 : 0] tmp_result;
+  reg         tmp_carry_out;
+  reg         tmp_zero;
+
+
+  //----------------------------------------------------------------
   // Concurrent connectivity for ports etc.
   //----------------------------------------------------------------
-  assign result = 8'h0;
-  assign carry  = 1'b1;
-  assign zero   = 1'b1;
+  assign result = tmp_result;
+  assign carry  = tmp_carry_out;
+  assign zero   = tmp_zero;
+
+
+  //----------------------------------------------------------------
+  //----------------------------------------------------------------
+  always @*
+    reg [8 : 0] tmp_add;
+    begin : alu
+      tmp_result    = 8'h0;
+      tmp_carry_out = 0;
+      tmp_zero      = 0;
+
+
+      case (operation)
+        OP_AND:
+          begin
+            tmp_result = a & b;
+          end
+
+        OP_OR:
+          begin
+            tmp_result = a | b;
+          end
+
+        OP_XOR:
+          begin
+            tmp_result = a ^ b;
+          end
+
+        OP_NOT:
+          begin
+            tmp_result = ~a;
+          end
+
+        OP_ASL:
+          begin
+            tmp_result = {a[6 : 0], carry_in};
+            tmp_carry_out = a[7];
+          end
+
+        OP_ROL:
+          begin
+            tmp_result = {a[6 : 0], a[7]};
+          end
+
+        OP_ASR:
+          begin
+            tmp_result = {carry_in, a[7 : 1]};
+            tmp_carry_out = a[0];
+          end
+
+        OP_ROR:
+          begin
+            tmp_result = {a[0], a[7 : 1]};
+          end
+
+        OP_ADD:
+          begin
+            tmp_add = a + b + carry_in;
+            tmp_result = tmp_add[7 : 0];
+            tmp_carry_out = tmp_add[8];
+          end
+
+        OP_INC:
+          begin
+            tmp_add = a + 1'b1;
+            tmp_result = tmp_add[7 : 0];
+            tmp_carry_out = tmp_add[8];
+          end
+
+        OP_SUB:
+          begin
+            tmp_result = a - b;
+            if (a == b)
+              tmp_zero = 1;
+          end
+
+        OP_DEC:
+          begin
+            tmp_result = a - 1'b1;
+          end
+
+        OP_CMP:
+          begin
+            if (a == b)
+              tmp_zero = 1;
+          end
+
+        default:
+          begin
+          end
+      endcase // case (operation)
+    end // alu
+
 
 endmodule // 6502_alu
 
