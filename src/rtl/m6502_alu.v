8b6d0f150d8184f9f468604ef931ea493490d984
Joachim Strömbergson <joachim@secworks.se>
2016-08-20 17:15:57 +0200

    Fixed syntax errors in the alu.

--- src/rtl/m6502_alu.v
+++ src/rtl/m6502_alu.v
@@ -44,9 +44,9 @@ module m6502_alu(
                  input wire  [7 : 0] op_b,
                  input wire          carry_in,
                  output wire [7 : 0] result,
-                 output wire         carry_out,
-                 output wire         overflow,
-                 output wire         zero
+                 output wire         carry,
+                 output wire         zero,
+                 output wire         overflow
                 );
 
   //----------------------------------------------------------------
@@ -74,100 +74,101 @@ module m6502_alu(
   // Wires.
   //----------------------------------------------------------------
   reg [7 : 0] tmp_result;
-  reg         tmp_carry_out;
+  reg         tmp_carry;
   reg         tmp_zero;
-
+  reg         tmp_overflow;
 
   //----------------------------------------------------------------
   // Concurrent connectivity for ports etc.
   //----------------------------------------------------------------
-  assign result = tmp_result;
-  assign carry  = tmp_carry_out;
-  assign zero   = tmp_zero;
-
+  assign result   = tmp_result;
+  assign carry    = tmp_carry;
+  assign zero     = tmp_zero;
+  assign overflow = tmp_overflow;
 
   //----------------------------------------------------------------
   //----------------------------------------------------------------
   always @*
-    reg [8 : 0] tmp_add;
     begin : alu
-      tmp_result    = 8'h0;
-      tmp_carry_out = 0;
-      tmp_zero      = 0;
+      reg [8 : 0] tmp_add;
 
+      tmp_result   = 8'h0;
+      tmp_carry    = 0;
+      tmp_zero     = 0;
+      tmp_overflow = 0;
 
       case (operation)
         OP_AND:
           begin
-            tmp_result = a & b;
+            tmp_result = op_a & op_b;
           end
 
         OP_OR:
           begin
-            tmp_result = a | b;
+            tmp_result = op_a | op_b;
           end
 
         OP_XOR:
           begin
-            tmp_result = a ^ b;
+            tmp_result = op_a ^ op_b;
           end
 
         OP_NOT:
           begin
-            tmp_result = ~a;
+            tmp_result = ~op_a;
           end
 
         OP_ASL:
           begin
-            tmp_result = {a[6 : 0], carry_in};
-            tmp_carry_out = a[7];
+            tmp_result = {op_a[6 : 0], carry_in};
+            tmp_carry  = op_a[7];
           end
 
         OP_ROL:
           begin
-            tmp_result = {a[6 : 0], a[7]};
+            tmp_result = {op_a[6 : 0], op_a[7]};
           end
 
         OP_ASR:
           begin
-            tmp_result = {carry_in, a[7 : 1]};
-            tmp_carry_out = a[0];
+            tmp_result = {carry_in, op_a[7 : 1]};
+            tmp_carry  = op_a[0];
           end
 
         OP_ROR:
           begin
-            tmp_result = {a[0], a[7 : 1]};
+            tmp_result = {op_a[0], op_a[7 : 1]};
           end
 
         OP_ADD:
           begin
-            tmp_add = a + b + carry_in;
+            tmp_add = op_a + op_b + carry_in;
             tmp_result = tmp_add[7 : 0];
-            tmp_carry_out = tmp_add[8];
+            tmp_carry  = tmp_add[8];
           end
 
         OP_INC:
           begin
-            tmp_add = a + 1'b1;
+            tmp_add = op_a + 1'b1;
             tmp_result = tmp_add[7 : 0];
-            tmp_carry_out = tmp_add[8];
+            tmp_carry  = tmp_add[8];
           end
 
         OP_SUB:
           begin
-            tmp_result = a - b;
-            if (a == b)
+            tmp_result = op_a - op_b;
+            if (tmp_result == 8'h00)
               tmp_zero = 1;
           end
 
         OP_DEC:
           begin
-            tmp_result = a - 1'b1;
+            tmp_result = op_a - 1'b1;
           end
 
         OP_CMP:
           begin
-            if (a == b)
+            if (op_a == op_b)
               tmp_zero = 1;
           end
 
@@ -176,7 +177,6 @@ module m6502_alu(
           end
       endcase // case (operation)
     end // alu
-
 
 endmodule // 6502_alu
 
